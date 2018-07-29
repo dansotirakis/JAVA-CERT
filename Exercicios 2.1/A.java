c6a6400320187e9fada95b4e8f18b3a72a8afe07
Damianos Sotirakis <dansotirakis18@gmail.com>
2018-07-28 23:32:56 -0300

    Exercicio 10

--- Exercicios 2.1/A.java
+++ Exercicios 2.1/A.java
@@ -1,7 +1,8 @@
 class A{
 	public static void main (String[] args){
-		for (char c ='a'; c <= 'z'; c++){
-			System.out.println(c);
+		boolean BOOLEAN = false;
+		if (BOOLEAN){
+			System.out.println("Yes");
 		}
 	}
 }
