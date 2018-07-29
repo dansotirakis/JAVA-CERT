33c5389cc034b8685c0461c570256e4fe0754155
Damianos Sotirakis <dansotirakis18@gmail.com>
2018-07-29 12:58:08 -0300

    Exercicio 2.2 -2

--- Exercicios 2.2/A.java
+++ Exercicios 2.2/A.java
@@ -1,11 +1,14 @@
+class B{
+	int v = 15;
+}
 class A{
 	public static void main(String[] args){
-		int x = 15;
-		int y = x;
-		y++;
-		x++;
-		int z = y;
-		z--;
-		System.out.println(x+ y + z);
+		B x = 15;
+		B y = x;
+		y.v++;
+		x.v++;
+		B z = y;
+		z.v--;
+		System.out.println(x.v+ y.v + z.v);
 	}
 }
