9ceadf3afeeb9d696546a84b34a9c42a807ae475
Damianos Sotirakis <dansotirakis18@gmail.com>
2018-07-28 22:38:10 -0300

    Exercicio 2.1-7

--- Exercicios 2.1/A.java
+++ Exercicios 2.1/A.java
@@ -1,10 +1,8 @@
 class A{
 	public static void main (String[] args){
-		boolean argis;
-		if(args.length > 0)
-			argis = 1;
-		else
-			argis = 0;
-		System.out.println(argis);
+		int n = 09;
+		int m = 03;
+		int x = 1_000;
+		System.out.println(x - n + m);
 	}
 }
