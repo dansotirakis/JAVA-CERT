d2421133148baa53b8150d0e2cccb0d117f7a8e5
Damianos Sotirakis <dansotirakis18@gmail.com>
2018-07-29 15:00:59 -0300

    Exercicio 2.3 - 1

---
+++ Exercicios 2.3/A.java
@@ -0,0 +1,15 @@
+class B{
+	int c;
+	void c(int c) {
+		c = c;
+	}
+}
+class A{
+	public static void main (String[] args){
+		B b = new B();
+		b.c = 10;
+		System.out.println(b.c);
+		b.c(30);
+		System.out.println(b.c);
+	}
+}
