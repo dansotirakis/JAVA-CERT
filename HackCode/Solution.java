8b5f9e981565d6798b0cd7f7ff0c7c73cdfa49af
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-08-04 19:21:00 -0300

    At-Revision

---
+++ HackCode/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    static int sockMerchant(int n, int[] ar) {
+       n++;
+    return n;
+	}
+
+    public static void main(String[] args) {
+
+        int n = 9;
+		int ar[] = new int[n];
+		for (int i =0; i<=9; i++){
+			cont 5
+		}
+	
+        //int result = sockMerchant(n, ar);
+		System.out.println(ar);
+    }
+}
+qx
