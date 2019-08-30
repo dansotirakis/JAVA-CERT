1eae9f462360b104fe446484b1a1d8dda72e324e
Damianos Sotirakis <dansotirakis18@gmail.com>
2019-08-30 00:55:06 -0300

    Update Solution.java

--- HackCode/Solution.java
+++ HackCode/Solution.java
@@ -1,37 +1,36 @@
 import java.util.Scanner;
+
 public class Solution {
-	
-	
-	
     static int sockMerchant(int n, int[] ar) {
-    	int ard[][] = new int[n][2];
+
     	int pares = 0;
     	int rep = 0;
-    	
+		int ard[][] = new int[n][2];
+
     	for( int c = ar.length-1 ; c >= 0 ; c-- ) {
-           	ard[c][0] = ar[c];
-           	for( int t = ar.length-1 ; t >= 0 ; t-- ){
-           		for (int l = ard.length-1; l >=0 ; l--) {
-	           		if(ard[l][0] != ar[t]) {
-	           			rep = 0; 
-	           		}
-	           		else { 
-	           			rep = 1;
-	           		}
-           		}
-           		if(rep == 0) {
-	           		if(ard[c][0] == ar[t]) {
-	           			ard[t][1] = (ard[t][1]+1);
-	           		}
-           		}
-           	}
+
+    		for(int cont = ar.length-1 ; cont >= 0 ; cont --) {
+				if (ard[c][0] != ar[c]) {
+					ard[c][0] = ar[c];
+				}
+			}
+
+			for(int cont1 = ar.length-1 ; cont1 >= 0 ; cont1 --) {
+				if (ard[cont1][0] == ar[cont1]) {
+					ard[cont1][1] = (ard[cont1][1] + 1);
+				}
+			}
         	
         }
-    	for(int c = ard.length-1; c >= 0 ; c--) {
-    		pares = pares + ard[c][1];
+    	for(int cont3 =  ard.length-1; cont3 >= 0 ; cont3--) {
+    		pares = pares + ard[cont3][1];
     	}
-    	
-		return pares/8;
+
+		if(pares % 2 == 0){
+			return pares;
+		}else{
+			return pares;
+		}
 	}
 
     public static void main(String[] args) {
@@ -39,7 +38,7 @@ public class Solution {
         int n = 9;
         int ar[] = new int[n];
         
-        for( int c = 8 ; c >= 0 ; c-- ) {
+        for( int c = ar.length-1 ; c >= 0 ; c-- ) {
         	
         	System.out.println("Insira um valor");
         	Scanner i = new Scanner(System.in);
